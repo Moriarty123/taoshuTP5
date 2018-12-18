34228d9e6c90b5937a1c10c95b28450878dc1725
Moriarty123 <1561357687@qq.com>
2018-12-19 01:05:30 +0800

    2018-12-19 admin/admin/updatePwd 修改密码
    Signed-off-by: Moriarty123 <1561357687@qq.com>

--- ThinkPHP/application/admin/validate/admin.php
+++ ThinkPHP/application/admin/validate/admin.php
@@ -7,19 +7,27 @@ use think\Validate;
 class admin extends Validate
 {
 	protected $rule = [
-        'name|用户名'  =>  'require|max:25',
-        'pwd|密码' =>  'require|min:5',
+        'name|用户名'      =>  'require|max:25',
+        'prepwd|原始密码'  =>  'require|min:5',
+        'pwd|密码'         =>  'require|min:5',
+        'repwd|确认密码'   =>  'require|min:5|confirm:pwd',
     ];
     
     protected $message = [
-        'name.require'  =>  '用户名必须填写',
-        'name.max'	=>	'用户名最多25位',
-        'pwd.require' =>  '密码必须填写',
-        'pwd.min'	=>	'密码最少5位'
+        'name.require'      =>  '用户名必须填写',
+        'name.max'	        =>	'用户名最多25位',
+        'prepwd.require'    =>  '原始密码必须填写',
+        'prepwd.min'        =>  '原始密码最少5位',
+        'pwd.require'       =>  '密码必须填写',
+        'pwd.min'	        =>	'密码最少5位',
+        'repwd.require'     =>  '确认密码必须填写',
+        'repwd.min'         =>  '确认密码最少5位',
+        'repwd.confirm'     =>  '两次密码不一致',
     ];
     
     protected $scene = [
-        // 'add'   =>  ['name','email'],
-        // 'edit'  =>  ['email'],
+        'add'   =>  ['name', 'pwd', 'prepwd', 'repwd'],
+        'edit'  =>  ['pwd', 'prepwd', 'repwd'],
+        'login' =>  ['name', 'pwd'],
     ];
 }
