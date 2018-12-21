519cd987f0db7f46bbdc7c645c65e6afb8a696db
Moriarty123 <1561357687@qq.com>
2018-12-21 23:09:20 +0800

    2018-12-21 添加新用户 已完成
    Signed-off-by: Moriarty123 <1561357687@qq.com>

---
+++ ThinkPHP/application/admin/controller/Common.php
@@ -0,0 +1,34 @@
+<?php
+namespace app\admin\controller;
+
+use think\Db;
+use think\Controller;
+
+class Common extends Controller
+{
+    //上传图片
+	public function UserImage() 
+	{
+		// dump($_POST);
+		// dump($_FILES);
+		$file = request()->file('image');
+		// dump($file);
+		if ($file == NULL) {
+			$this->error('未选择图片');
+		}
+		
+		//1.2 移动到框架应用根目录/public/uploads/ 目录下
+		if($file){
+			$info = $file->move(ROOT_PATH . 'public' . DS . 'uploads'.DS.'user');
+			if($info){
+				// 成功上传后 获取上传信息
+				$img_path = "/uploads/user/{$info->getSaveName()}";
+				echo $img_path;
+				// $this->success('上传成功!'.$img_path);
+			}else{
+				// 上传失败获取错误信息
+				$this->error($file->getError());
+			}
+		}
+	}   
+}
--- ThinkPHP/application/admin/controller/User.php
+++ ThinkPHP/application/admin/controller/User.php
@@ -35,7 +35,50 @@ class User extends Controller
 
 	public function userAdd()
 	{
-		dump($_POST);
+		// dump($_POST);
+
+		//1.获取数据
+		$name = input('post.name');
+		$pwd = input('post.pwd');
+		$sex = input('post.sex');
+		$realname = input('post.realname');
+		$img = input('post.headImg');
+		$tel = input('post.tel');
+		$addr = input('post.addr');
+		$grade = input('post.grade');
+		$class_id = input('post.class');
+		$content = input('post.content');
+
+		//2.构造数据
+		$data = [
+			'user_name'		=> 	$name,
+			'user_pwd'		=> 	md5($pwd),
+			'user_sex'		=> 	$sex,
+			'user_realname'	=>	$realname,
+			'user_img'		=> 	$img,
+			'user_tel'		=> 	$tel,
+			'user_addr'		=> 	$addr,
+			'grade'			=> 	$grade,
+			'class_id'		=> 	$class_id,
+			'user_content'	=> 	$content
+		];
+
+		//3.存入数据库
+		$where = "user_name = {$name}";
+		$res = Db::table('user')->where($where)->find();
+
+		if (!empty($res)) {
+			$this->error('该用户已注册');
+		}
+		$ret = Db::table('user')->insert($data);
+
+		if ($ret == false) {
+			$this->error('添加用户失败');
+
+		}
+
+		//4.后续操作
+		$this->success('添加成功', '/admin/user/userList');
 	}
 
 }
