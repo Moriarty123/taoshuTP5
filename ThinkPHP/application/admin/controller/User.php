cf3c3dc15803758903f7e67e836091924c170285
Moriarty123 <1561357687@qq.com>
2018-12-22 01:33:20 +0800

    2018-12-22 修改用户
    Signed-off-by: Moriarty123 <1561357687@qq.com>

--- ThinkPHP/application/admin/controller/User.php
+++ ThinkPHP/application/admin/controller/User.php
@@ -25,14 +25,30 @@ class User extends Controller
 	}
 
 	//添加用户信息填写
-	public function userPage()
+	public function addPage()
 	{
-		$class = Db::table('class')->select();
+		$classes = Db::table('class')->select();
 
-		$this->assign('class', $class);
+		$this->assign('classes', $classes);
 		return $this->fetch('userAdd');
 	}
 
+	//编辑用户信息填写
+	public function editPage()
+	{
+		//查找特定ID的用户
+		$user_id = input('get.user_id');
+		$where = "user_id = {$user_id}";
+		$user = Db::table('user')->where($where)->find();
+		$this->assign('user', $user);
+
+		$classes = Db::table('class')->select();
+		$this->assign('classes', $classes);
+
+		return $this->fetch('userEdit');
+	}
+
+	//添加用户
 	public function userAdd()
 	{
 		// dump($_POST);
@@ -79,6 +95,51 @@ class User extends Controller
 
 		//4.后续操作
 		$this->success('添加成功', '/admin/user/userList');
+	}
+
+	//更新用户信息
+	public function userUpdate()
+	{
+		// dump($_POST);
+		//1.获取数据
+		$id = input('post.id');
+		$name = input('post.name');
+		$pwd = input('post.pwd');
+		$sex = input('post.sex');
+		$realname = input('post.realname');
+		$img = input('post.headImg');
+		$tel = input('post.tel');
+		$addr = input('post.addr');
+		$grade = input('post.grade');
+		$class_id = input('post.class');
+		$content = input('post.content');
+
+		//2.构造数据
+		$data = [
+			'user_name'		=> 	$name,
+			'user_pwd'		=> 	md5($pwd),
+			'user_sex'		=> 	$sex,
+			'user_realname'	=>	$realname,
+			'user_img'		=> 	$img,
+			'user_tel'		=> 	$tel,
+			'user_addr'		=> 	$addr,
+			'grade'			=> 	$grade,
+			'class_id'		=> 	$class_id,
+			'user_content'	=> 	$content
+		];
+
+		//3.存入数据库
+		$where = "user_id = {$id}";
+		
+		$ret = Db::table('user')->where($where)->update($data);
+
+		if ($ret == false) {
+			$this->error('更新用户信息失败');
+
+		}
+
+		//4.后续操作
+		$this->success('更新用户信息成功', '/admin/user/userList');
 	}
 
 }
