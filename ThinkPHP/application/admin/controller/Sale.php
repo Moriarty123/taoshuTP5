c799dd54a78d9d85a2616382247ec15fb38bcb9f
Moriarty123 <1561357687@qq.com>
2018-12-23 17:56:33 +0800

    2018-12-23 出售书籍模糊搜索
    Signed-off-by: Moriarty123 <1561357687@qq.com>

--- ThinkPHP/application/admin/controller/Sale.php
+++ ThinkPHP/application/admin/controller/Sale.php
@@ -32,5 +32,43 @@ use think\Db;
 		return $this->fetch('saleBookList');
 	}
 
+	//模糊查找
+	public function saleBookSearch()
+	{
+		// dump($_POST);
+		$search = input('post.search');
+
+		if(!empty($search)) {
+			session('salesearch', $search);
+			$where['sale_name'] = array('like','%'.$search.'%');//封装模糊查询 赋值到数组	
+		}
+		else 
+		{
+			$search = session('salesearch');
+			$where['sale_name'] = array('like','%'.$search.'%');	
+		}
+
+		
+		//级联查询
+		$saleBookList = Db::table('sale_book')
+		->where($where)
+		->alias('a')
+		->join('type_second b', 'a.sale_secondtype = b.second_id')
+		->join('user c', 'a.user_id = c.user_id')
+		->paginate(15);
+
+		$bookNumber = Db::table('sale_book')
+		->where($where)
+		->alias('a')
+		->join('type_second b', 'a.sale_secondtype = b.second_id')
+		->join('user c', 'a.user_id = c.user_id')
+		->count();
+
+		$this->assign('saleBookList', $saleBookList);
+		$this->assign('bookNumber', $bookNumber);
+
+		return $this->fetch('saleBookList');
+
+	}
 
 }
