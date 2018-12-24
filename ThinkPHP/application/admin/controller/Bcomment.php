07d2e6419cd3f012d0a6fec8229becec2852bc84
Moriarty123 <1561357687@qq.com>
2018-12-24 17:11:18 +0800

    2018-12-24	添加新出售留言
    Signed-off-by: Moriarty123 <1561357687@qq.com>

--- ThinkPHP/application/admin/controller/Bcomment.php
+++ ThinkPHP/application/admin/controller/Bcomment.php
@@ -12,6 +12,7 @@ class Bcomment extends Controller
 		return $this->fetch('index');
 	}
 
+	//出售书籍留言列表
 	public function commentList()
 	{
 		$commentList = Db::table('bcomment')
@@ -30,6 +31,49 @@ class Bcomment extends Controller
 		$this->assign('commentNumber', $commentNumber);
 
 		return $this->fetch('commentList');
+	}
+
+	//添加页面
+	public function addPage()
+	{
+		$users = Db::table('user')->select();
+		$sale_book = Db::table('sale_book')->select();
+
+		$this->assign('users', $users);
+		$this->assign('sale_book', $sale_book);
+
+		return $this->fetch('commentAdd');
+	}
+
+	//添加留言
+	public function commentAdd()
+	{
+		// dump($_POST);
+
+		//1.获取数据
+		$user_id = input('post.user_id');
+		$sale_id = input('post.sale_id');
+		$content = input('post.content');
+		$time = time();
+
+		//2.构造数据
+		$data = [
+			'user_id'			=> $user_id,
+			'bcomment_content'	=> $content,
+			'bbook_id'			=> $sale_id,
+			'bcomment_time'		=> $time
+		];
+		
+		//3.存入数据库
+		$ret = Db::table('bcomment')->insert($data);
+
+		if ($ret == false) {
+			$this->error('添加留言失败！');
+		}
+
+		//4.后续操作
+		$this->success('添加留言成功！', '/admin/bcomment/commentList');
+
 	}
 
 }
