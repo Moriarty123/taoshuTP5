283ff52aeb22d96a6073b4e7d61aaaff4b083364
Moriarty123 <1561357687@qq.com>
2018-12-25 16:17:47 +0800

    2018-12-25 添加订单
    Signed-off-by: Moriarty123 <1561357687@qq.com>

--- ThinkPHP/application/admin/controller/Order.php
+++ ThinkPHP/application/admin/controller/Order.php
@@ -12,13 +12,14 @@ class Order extends Controller
 		return $this->fetch('index');
 	}
 
-	public function order()
+	public function orderList()
 	{
 		//获取订单
 		$order = Db::table('shoporder')
 		->alias('a')
 		->join('user b', 'a.user_id = b.user_id')
 		->join('sale_book c', 'a.book_id = c.sale_id')
+		->order('create_time','desc')
 		->paginate(15);
 
 		$orderNumber = Db::table('shoporder')
@@ -30,7 +31,58 @@ class Order extends Controller
 		$this->assign('order', $order);
 		$this->assign('to', $orderNumber);
 
-		return $this->fetch('order');
+		return $this->fetch('orderList');
+	}
+
+	//添加订单填写页面
+	public function addPage()
+	{
+		$user = Db::table('user')->select();
+		$sale_book = Db::table('sale_book')->select();
+
+		$this->assign('user', $user);
+		$this->assign('book', $sale_book);
+
+		return $this->fetch('orderAdd');
+	}
+
+	//添加订单
+	public function orderAdd()
+	{
+		// dump($_POST);
+		//1.获取数据
+		$user_id 	= input('post.user');
+		$sale_id 	= input('post.name');
+		$num 		= input('post.num');
+		$price 		= input('post.price');
+
+		$order_id = date("Y-m-dH-i-s");
+		$order_id = str_replace("-","",$order_id);
+		$order_id .= $user_id;
+		$order_id .= rand(100000,999999);
+
+		//2.构造数据
+		$data = [
+			'order_id'		=> $order_id,
+			'user_id' 		=> $user_id,
+			'book_id' 		=> $sale_id,
+			'order_sum' 	=> $num,
+			'order_price' 	=> $price,
+			'create_time'	=> time(),
+			'order_state'	=> 0,
+			'order_buy'		=> 0,
+			'order_sale'	=> 0
+		];
+		// dump($data);
+		// die();
+		//3.存入数据库
+		$ret = Db::table('shoporder')->insert($data);
+
+		if ($ret == false) {
+			$this->error('添加订单失败！');
+		}
+
+		$this->success('添加订单成功！', '/admin/order/orderList');
 	}
 
 
