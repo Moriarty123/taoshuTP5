e2abb46b8c6708c76b8138a0bf37729c9fbe59cb
Moriarty123 <1561357687@qq.com>
2018-12-26 15:45:09 +0800

    2018-12-26 删除公告
    Signed-off-by: Moriarty123 <1561357687@qq.com>

--- ThinkPHP/application/admin/controller/Notice.php
+++ ThinkPHP/application/admin/controller/Notice.php
@@ -101,5 +101,24 @@ use think\Db;
 		return $this->fetch('noticeList');
 
 	}
+
+	public function noticeDelete()
+	{
+		// dump($_GET);
+
+		//1.
+		$notice_id = input('get.notice_id');
+		$where = "notice_id = {$notice_id}";
+		
+		//2.
+		$ret = Db::table('notice')->where($where)->delete();
+
+		//3.
+		if ($ret == false) {
+			$this->error('删除公告失败！', '/admin/notice/noticeList');
+		}
+
+		$this->success('删除公告成功！', '/admin/notice/noticeList');
+	}
 }
 
