a33bbee53eaf4a55981007b60da8056206081217
Moriarty123 <1561357687@qq.com>
2018-12-26 15:38:05 +0800

    2018-12-26 添加新公告
    Signed-off-by: Moriarty123 <1561357687@qq.com>

--- ThinkPHP/application/admin/controller/Notice.php
+++ ThinkPHP/application/admin/controller/Notice.php
@@ -31,5 +31,75 @@ use think\Db;
 		return $this->fetch('noticeList');
 	}
 
+	public function addPage()
+	{
+		return $this->fetch('noticeAdd');
+	}
 
+	//添加公告
+	public function noticeAdd()
+	{
+		// dump($_POST);
+
+		//1.
+		$title = input('post.title');
+		$content = input('post.content');
+		$admin_id = session('admin_id');
+
+		//2.
+		$data = [
+			'notice_title'	=> $title,
+			'notice_content'=> $content,
+			'notice_time'	=> time(),
+			'admin_id'		=> $admin_id,
+			'view_time'		=> 0
+		];
+
+		//3.
+		$ret = Db::table('notice')->insert($data);
+
+		if ($ret == false) {
+			$this->error('添加新公告失败！', '/admin/notice/noticeList');
+		}
+
+		$this->success('添加新公告成功！', '/admin/notice/noticeList');
+
+	}
+
+
+	//模糊查找
+	public function noticeSearch()
+	{
+		// dump($_POST);
+		$search = input('post.search');
+
+		if(!empty($search)) {
+			session('noticesearch', $search);
+			$where['notice_title'] = array('like','%'.$search.'%');//封装模糊查询 赋值到数组	
+		}
+		else 
+		{
+			$search = session('noticesearch');
+			$where['notice_title'] = array('like','%'.$search.'%');	
+		}
+		
+		$noticeList = Db::table('notice')
+		->where($where)
+		->alias('a')
+		->join('admin b', 'a.admin_id = b.id')
+		->paginate(15);
+
+		$noticeNumber = Db::table('notice')
+		->where($where)
+		->alias('a')
+		->join('admin b', 'a.admin_id = b.id')
+		->count();
+
+		$this->assign('notice', $noticeList);
+		$this->assign('to', $noticeNumber);
+
+		return $this->fetch('noticeList');
+
+	}
 }
+
